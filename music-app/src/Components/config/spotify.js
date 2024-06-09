efa18343fbb5c8af232d923fbf70e2a4761fffdd
swaminathan33 <swamiandems@gmail.com>
2024-06-10 03:27:49 +0530

    update

--- music-app/src/Components/config/spotify.js
+++ music-app/src/Components/config/spotify.js
@@ -1,6 +1,5 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
-// const redirectUrl = 'http://localhost:5173/signin'
-const redirectUrl = 'https://music-app-11.netlify.app/signin'
+const redirectUrl = 'http://localhost:5173/signin'
 // const redirectUrl = 'https://localhost:3000/callback/'
 const clientId = 'de8618a5edbc4f39a21bfd32c66040f2'
 
@@ -11,10 +10,9 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ]
-const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join(
     "%20"
   )}&response_type=token&show_dialog=true`;
 
-console.log(loginUrl)
-
-export default loginUrl;
+console.log(loginUrl);
