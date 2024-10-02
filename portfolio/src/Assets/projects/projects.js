30696224ae06215b52d1c7fed7560487c87435c0
swaminathan33 <swamiandems@gmail.com>
2024-10-02 20:38:25 +0530

    updated portfolio

---
+++ portfolio/src/Assets/projects/projects.js
@@ -0,0 +1,49 @@
+import Dashboard from './dashboard.png'
+import Ecommerce from './ecommerce.png'
+import Jobhuntz from './jobhuntz.png'
+import Todoapp from './todoapp.png'
+import Youtube from './youtube.png'
+import Portfolio from './portfolio.png'
+
+const projects = [
+    {
+        id:0,
+        name:'Dashboard App',
+        image:Dashboard,
+        link:'https://geex-react.netlify.app/'
+    },
+    {
+        id:1,
+        name:'Job Searching App',
+        image:Jobhuntz,
+        link:'https://jobhuntz.netlify.app/'
+    },
+    {
+        id:2,
+        name:'Todo App',
+        image:Todoapp,
+        link:'https://dailytodo1.netlify.app/register'
+    },
+    {
+        id:3,
+        name:'Youtube Clone App',
+        image:Youtube,
+        link:'https://youtubesiteclone.netlify.app/'
+    },
+    {
+        id:4,
+        name:'Portfolio App',
+        image:Portfolio,
+        link:'https://swaminathan.netlify.app/'
+    },
+    {
+        id:5,
+        name:'Ecommerce App',
+        image:Ecommerce,
+        link:'https://ecommercesite1.netlify.app/'
+    },
+    
+]
+
+
+export default projects
--- portfolio/src/Pages/Projects.jsx
+++ portfolio/src/Pages/Projects.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { FaLaptopCode } from "react-icons/fa";
 import Sample from "../Assets/sample.jpeg";
 import { Tilt } from "@jdion/tilt-react";
+import projects from "../Assets/projects/projects";
 
 const Projects = () => {
-  const projects = [
-    {
-      id: 0,
-      name: "Insta CLone",
-      desc: "Instagram clone with x tech and y tech deployed in abc website",
-      live_link: "#",
-      github_link: "#",
-      image: "#",
-    },
-  ];
+  // const projects = [
+  //   {
+  //     id: 0,
+  //     name: "Insta CLone",
+  //     desc: "Instagram clone with x tech and y tech deployed in abc website",
+  //     live_link: "#",
+  //     github_link: "#",
+  //     image: "#",
+  //   },
+  // ];
   return (
     <div className="bg-indigo-950 py-7 " id="projects">
       <div className="text-white flex items-center gap-3 justify-center font-bold text-3xl py-3 pb-10">
@@ -23,7 +24,7 @@ const Projects = () => {
         <span>Projects Made</span>
       </div>
       <div className="flex flex-wrap gap-4 justify-center px-20">
-        {[1, 2, 3, 4, 5, 6].map((i) => {
+        {/* {[1, 2, 3, 4, 5, 6].map((i) => {
           return (
             <Tilt>
               <div className="">
@@ -32,6 +33,24 @@ const Projects = () => {
                   Instagram Clone
                 </div>
               </div>
+            </Tilt>
+          );
+        })} */}
+        {projects.map((i) => {
+          return (
+            <Tilt>
+              <a href={i.link} target="_blank">
+                <div className="">
+                  <img
+                    src={i.image}
+                    className=" w-80 h-52 rounded-t-md"
+                    alt=""
+                  />
+                  <div className="bg-yellow-400 font-bold text-lg p-2 rounded-b-md">
+                    {i.name}
+                  </div>
+                </div>
+              </a>
             </Tilt>
           );
         })}
